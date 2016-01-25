b445d42c34ae1dddd80455496d4a14b33fc36873
phy5 <phypolly@gmail.com>
2016-01-24 22:32:00 -0500

    adding assignment2 to repo

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates sepcial matrix object that can cache a matrix
 
-## Write a short comment describing this function
+## cacheSolve inverts the special matrix object returned by makeCacheMatrix. If the inverse is already calculated, 
+## then cacheSolve retrieves the inverse from the cache. If the inverse is not available, cacheSolve uses solve 
+## function to invert the matrix 
+
+
+
+## Creates a sepcial matrix object that can cache a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	Invertedmatrix <- NULL
+	set <- function(y) {
+		x <<- y
+		Invertedmatrix <<- NULL
+	}
+	get <- function() x
+	setmatrix <- function(si) Invertedmatrix <<- si
+	getmatrix <- function() Invertedmatrix 
+	list(set = set, get = get,
+	setmatrix = setmatrix,
+	getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## Returns a matrix that is the inverse of the matrix created by the makeCacheMatix function. if the inverse is
+## available, it will return the inverse from the cache. If not, use solve to invert the matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x , ...) {
+	invmat <- x$getmatrix()
+	if (!is.null(invmat)) {
+		message("getting cached data")
+		return(invmat)
+	}
+
+	origmatrix <- x$get()
+	invmat <- solve(origmatrix, ...)
+	x$setmatrix(invmat)
+	invmat
 }
